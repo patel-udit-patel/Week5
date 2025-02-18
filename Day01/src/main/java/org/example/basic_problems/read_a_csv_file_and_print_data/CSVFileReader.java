2752350a488fd462c65069124bf02043f6338a32
patel-udit-patel <udit0428t@gmail.com>
2025-02-18 13:02:49 +0530

    [Udit Patel] Add. Read a CSV File and Print Data on 18-02-25

---
+++ Day01/src/main/java/org/example/basic_problems/read_a_csv_file_and_print_data/CSVFileReader.java
@@ -0,0 +1,46 @@
+package org.example.basic_problems.read_a_csv_file_and_print_data;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+//CSVFileReader-->reads data from CSV file and prints on console
+public class CSVFileReader {
+    //method reads and return data of a CSV file in String
+    public static ArrayList<String> readCSVFile(String filePath){
+        ArrayList<String>data=new ArrayList<>();
+        //Handling file exceptions
+        try(CSVReader reader=new CSVReader(new FileReader(filePath))){
+            String[] nextLine;
+            reader.readNext();
+            String obj="";
+            while((nextLine= reader.readNext())!=null){
+                obj=obj+("ID: "+nextLine[0]+ ",Name: "+nextLine[1]+",Age: "+nextLine[2]+",Marks:"+nextLine[3]);
+                data.add(obj);
+            }
+        }catch (CsvValidationException e){
+            System.out.println("CSV file not found");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return data;
+    }
+    public static void printData(ArrayList<String>data){
+        for(int i=0;i<data.size();i++){
+            System.out.println(data.get(i));
+        }
+    }
+    public static void main(String[] args) {
+        //File path of the given csv file
+        String filePath="C:\\Users\\udit0\\OneDrive\\Documents\\CapgeminiTraining\\Assignments\\Week5\\Day01\\src\\main\\java\\org\\example\\basic_problems\\read_a_csv_file_and_print_data\\student.csv";
+        //method call for readCSVFile
+        ArrayList<String>data=readCSVFile(filePath);
+
+        //Printing data
+        printData(data);
+    }
+}
---
+++ Day01/src/test/java/basic_problem_testing/CSVFileReaderTest.java
@@ -0,0 +1,17 @@
+package basic_problem_testing;
+import org.example.basic_problems.read_a_csv_file_and_print_data.CSVFileReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CSVFileReaderTest {
+
+    @Test
+    public  void readCSVFileTest(){
+        String filePath="C:\\Users\\udit0\\OneDrive\\Documents\\CapgeminiTraining\\Assignments\\Week5\\Day01\\src\\main\\java\\org\\example\\basic_problems\\read_a_csv_file_and_print_data\\student.csv";
+        ArrayList<String>data=CSVFileReader.readCSVFile(filePath);
+        ArrayList<String>expectedData=new ArrayList<>(Arrays.asList("ID: 123,Name: Vinay,Age: 21,Marks:50"));
+        Assertions.assertEquals(expectedData,data);
+    }
+}
